e42a4e98e898f669146bf6f9205b38e4d16391b5
Ernest Micklei <ernest.micklei@gmail.com>
2024-07-08 09:12:24 +0200

    allow test example 300 sec

--- examples/inside_test/inside_test.go
+++ examples/inside_test/inside_test.go
@@ -6,7 +6,8 @@ import (
 	"github.com/emicklei/structexplorer"
 )
 
+// go test -timeout 300s -run ^TestExplore$ github.com/emicklei/structexplorer/examples/inside_test
 func TestExplore(t *testing.T) {
-	// lets explore the "t" ; do it within 30 seconds before it timeouts
+	// lets explore the "t" ; do it within 300 seconds before it timeouts
 	structexplorer.NewService("testing.T", t).Start()
 }
