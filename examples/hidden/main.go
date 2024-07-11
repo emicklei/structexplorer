845f4130f2b641020fffdcb094a176179c453968
Ernest Micklei <ernest.micklei@gmail.com>
2024-07-11 11:18:08 +0200

    update example

--- examples/hidden/main.go
+++ examples/hidden/main.go
@@ -8,17 +8,17 @@ import (
 )
 
 type hidden struct {
-	private  bool
-	secret   *time.Time
-	timeFunc func() time.Time
-	null     *int
-	m        map[string]int
+	private      bool
+	secret       *time.Time
+	timeFunc     func() time.Time
+	anIntPointer *int
+	m            map[string]int
 }
 
 func main() {
 	n := time.Now()
 	h := &hidden{private: true, secret: &n, timeFunc: time.Now,
-		null: nil, m: map[string]int{"answer": 42}}
+		anIntPointer: nil, m: map[string]int{"answer": 42}}
 	m := map[string]*hidden{
 		"one": h,
 		"two": h,
